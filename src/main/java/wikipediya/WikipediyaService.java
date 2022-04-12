71b0b7bc505ce48f5de8ea02cd070490f40b7361
Abror Allaberganov <83587031+AbrorbekAllaberganov@users.noreply.github.com>
2022-04-12 10:04:21 +0500

    Update WikipediyaService.java

--- src/main/java/wikipediya/WikipediyaService.java
+++ src/main/java/wikipediya/WikipediyaService.java
@@ -18,12 +18,7 @@ public class WikipediyaService {
         Gson gson=new Gson();
         OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url("https://google-search3.p.rapidapi.com/api/v1/crawl/q=Messi")
-                .get()
-                .addHeader("x-rapidapi-host", "google-search3.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "b8aa608876mshb8a6efa7542cd60p1bbb41jsn03c0ccd23533")
-                .build();
+       //REQUES
 
         try {
             Response response = client.newCall(request).execute();
